4ccb62094a6171f9a46580f71afdc41dd377a8d7
Boran <boran@goegetap.name>
2021-07-03 12:31:10 +0200

    sorting execution order by dir name

--- pull-all.sh
+++ pull-all.sh
@@ -1,1 +1,1 @@
-find -type d -name ".git" -exec bash -c 'cd $(dirname {}); echo -n "pulling into $(basename $(pwd)) : "; git pull --ff-only' \;
+find -type d -name ".git" | sort | while IFS= read -r repo; do pushd "$(dirname $repo)"; git pull -q --ff-only; popd; done
