879a287417099d9a1aa5d01f4fbbb0f99a9bd0c1
Boran Gögetap <boran@goegetap.name>
2022-04-04 15:41:37 +0200

    check

---
+++ import-webchunks-into-rdf.sh
@@ -0,0 +1,5 @@
+find -name '*-*' | while read j; do
+ chunk_name=$(basename $j)
+ echo -e "importing \033[1;33m" $chunk_name "\033[0m"
+ curl http://localhost/symwork.atlassian.net/browse/$chunk_name?transfer=turtle&reduce=all
+done
--- sync_from_kgo.sysmsuite.de_.sh
+++ sync_from_kgo.sysmsuite.de_.sh
@@ -1,1 +1,4 @@
-rsync --recursive --update --links --times --delete kgo.symsuite.de:/usr/home/kgosymsu/public_html/.webchunks/ . 
+echo Starting to sync...
+rsync --recursive --update --links --times --delete kgo.symsuite.de:/usr/home/kgosymsu/public_html/.webchunks/$(basename $(pwd))/ . 
+echo ... done.
+du -h
