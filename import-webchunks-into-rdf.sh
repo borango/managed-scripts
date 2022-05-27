5052bdb971c1ea2b6ce7fd4012ba6a6910aa950f
Boran Gögetap <boran@goegetap.name>
2022-05-27 23:10:18 +0200

    check

--- import-webchunks-into-rdf.sh
+++ import-webchunks-into-rdf.sh
@@ -1,5 +1,13 @@
-find -name '*-*' | while read j; do
+unalias ls
+
+# find -name   '*-*' | \
+# ls -A1tr  -I '*-*' | \
+
+# list chunks with are not of *-* pattern and also skip trash/ folder
+  ls -A1trp -I '*-*' | grep -v /$ | \
+while read j; do
  chunk_name=$(basename $j)
- echo -e "importing \033[1;33m" $chunk_name "\033[0m"
+ #echo -e "importing \033[1;33m" $chunk_name "\033[0m"
+ echo -n .
  curl http://localhost/symwork.atlassian.net/browse/$chunk_name?transfer=turtle&reduce=all
 done
