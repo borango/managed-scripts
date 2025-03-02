553daa25bd296230ede34857854e0fd01c989bc3
Boran Gögetap <boran@goegetap.name>
2025-03-02 03:10:20 +0100

    check

--- bogo-termux-packages.sh
+++ bogo-termux-packages.sh
@@ -1,19 +1,22 @@
 apt install git
-apt install vim
-apt install neovim
 apt install jq
 apt install nodejs-lts
+apt install proot-distro
+apt install screen
 
 apt install rsync
 apt install wget
-apt install man
-apt install which
-apt install bash-completion
-apt install silversearcher-ag
-apt install screen
-apt install proot-distro
+apt install netcat-openbsd
 apt install traceroute
 
+apt install bash-completion
+apt install man
+apt install which
+apt install silversearcher-ag
+apt install vim
+apt install neovim
+
+apt install python3
 apt install golang
 apt install cmake
 
