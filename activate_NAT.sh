5a2be5b2c505c3a41e722f082994f3f091a37f98
Boran <boran@goegetap.name>
2025-10-23 21:32:07 +0000

    new interface names

--- activate_NAT.sh
+++ activate_NAT.sh
@@ -1,4 +1,3 @@
-sudo iptables -t nat -A POSTROUTING -o wlp4s0 -j MASQUERADE
-sudo iptables -A FORWARD -o wlp4s0 -i enx0050b6c13fc4                                      -j ACCEPT
-sudo iptables -A FORWARD -i wlp4s0 -o enx0050b6c13fc4 -m state --state RELATED,ESTABLISHED -j ACCEPT
-
+sudo iptables -A POSTROUTING -o wwan0       -t nat       -j MASQUERADE
+sudo iptables -A FORWARD     -o wwan0 -i enx0050b6c13fc4 -j ACCEPT
+sudo iptables -A FORWARD     -i wwan0 -o enx0050b6c13fc4 -j ACCEPT -m state --state RELATED,ESTABLISHED
