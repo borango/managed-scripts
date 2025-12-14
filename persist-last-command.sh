56786cec5722dc94c246be8c08fa8d18d78efd71
Boran <boran@goegetap.name>
2025-12-14 23:51:57 +0000

    check

--- persist-last-command.sh
+++ persist-last-command.sh
@@ -2,5 +2,5 @@
 
 scriptfile=lastcommand.sh
 
-history | grep $1 | tail -n 2 | head -n 1 | sed 's/^[ ]*[0-9]\+[ ]*//' | tee > $scriptfile
+history | grep -F "$1" | tail -n 2 | head -n 1 | sed 's/^[ ]*[0-9]\+[ ]*//' | tee > $scriptfile
 chmod +x $scriptfile
